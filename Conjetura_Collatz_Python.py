0b1261ac391632d14306ef69830d486d4691d8ac
Patricia-Sotodosos <patricia.sotodosos@edu.uah.es>
2016-12-26 17:03:58 +0100

    Update Conjetura_Collatz_Python.py

--- Conjetura_Collatz_Python.py
+++ Conjetura_Collatz_Python.py
@@ -1,6 +1,7 @@
 print "Introduzca un numero para calcular su conjetura de Collatz: "
 numero = int(raw_input())
 print "resultado"
+if numero > 0:
 while numero !=1:
     
          if numero%2 == 0:
@@ -10,3 +11,5 @@ while numero !=1:
             numero=numero*3+1
             
          print numero
+ else: 
+    print "un numero positivo"
